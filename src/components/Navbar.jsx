191e9bccbc016b93bb4d0ce15e54f6f6b2538c91
roufkhalid93 <roufkhalid93@gmail.com>
2025-01-03 21:13:52 +0800

    update resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -120,7 +120,7 @@ export default function Navbar() {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            href='https://drive.google.com/file/d/1OWdMhRmOcIVOC4f7R5xqk3HEK9AA1bzQ/view?usp=sharing'
+                            href='https://drive.google.com/file/d/1ByjQlxU4X0RETJFEaZhLM2Uk3ZR3S5w0/view?usp=sharing'
                         >
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
